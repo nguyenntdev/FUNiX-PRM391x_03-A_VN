234cbeb3a774e9288e77efa99e6a3165ef7059f4
Nguyen Thien <kakangocthien109@gmail.com>
2022-06-15 14:02:38 +0700

    ...

--- app/src/main/java/com/nguyennt/animal/MainActivity.java
+++ app/src/main/java/com/nguyennt/animal/MainActivity.java
@@ -2,29 +2,56 @@ package com.nguyennt.animal;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.google.android.material.navigation.NavigationView;
+import com.nguyennt.animal.databinding.ActivityNavigationBinding;
+import com.nguyennt.animal.ui.gallery.GalleryFragment;
+import com.nguyennt.animal.ui.home.HomeFragment;
+import com.nguyennt.animal.ui.slideshow.SlideshowFragment;
 
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final int REQUEST_CODE = 333;
     private GridView gridView;
     GridAdapter gridAdapter;
     ArrayList<AnimalModel> listModel = new ArrayList<AnimalModel>();
+    private NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.gridView = findViewById(R.id.gridView);// Ok roi nha
-// dung chua
+        gridView = findViewById(R.id.gridView);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.nav_view);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        setSupportActionBar(toolbar);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
 
         listModel.add(new AnimalModel(false, R.drawable.ic_dog, "dog", "", R.drawable.bg_dog));
         listModel.add(new AnimalModel(false, R.drawable.ic_goose, "goose", "", R.drawable.bg_goose));
@@ -39,15 +66,18 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 //                listModel.get(position).liked = true;
 //                Toast.makeText(MainActivity.this, "Clicked item: " + listModel.get(position).liked, Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(view.getContext(), DetailActivity.class);
+//                Intent i = new Intent(view.getContext(), DetailActivity.class);
+//
+//                Bundle bundle = new Bundle();
+//                bundle.putInt("index", position);
+//
+//                bundle.putInt("image", listModel.get(position).resource);
+//                bundle.putString("title", listModel.get(position).name);
+//                i.putExtras(bundle);
+//                startActivityForResult(i, REQUEST_CODE);
 
-                Bundle bundle = new Bundle();
-                bundle.putInt("index", position);
-
-                bundle.putInt("image", listModel.get(position).resource);
-                bundle.putString("title", listModel.get(position).name);
-                i.putExtras(bundle);
-                startActivityForResult(i, REQUEST_CODE);
+//                Intent i = new Intent(view.getContext(), NavigationActivity.class);
+//                startActivity(i);
             }
         });
     }
@@ -64,6 +94,32 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Result: " + result, Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_content_navigation,
+//                        new HomeFragment()).commit();
+                SlideshowFragment fragment = new SlideshowFragment();
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment homeFragment = getSupportFragmentManager().findFragmentById(R.id.fragmenthome);
+                transaction.hide(homeFragment);
+                transaction.replace(R.id.fragmentslideshow, fragment).commit();
+                break;
+            case R.id.nav_gallery:
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_content_navigation,
+                        new GalleryFragment()).commit();
+                break;
+
+            case R.id.nav_slideshow:
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_content_navigation,
+                        new SlideshowFragment()).commit();
+                break;
+        }
+        return false;
     }
 }
 
