9318314db645a0d4843e86e7289871bda75cd9b7
nguyennt <kakangocthien109@gmail.com>
2022-08-21 12:06:13 +0700

    Changed structure

---
+++ app/src/main/java/com/nguyennt/animal/model/AnimalModel.java
@@ -0,0 +1,53 @@
+package com.nguyennt.animal.model;
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
+
+public class AnimalModel implements Parcelable {
+    public boolean liked;
+    public int resource;
+
+    public  String name;
+    public  String detail;
+    public  int photo;
+    public  ArrayList<Integer> listPhoto;
+
+    public AnimalModel(boolean like, int r, String name, String detail, int photo) {
+        this.liked = like;
+        this.resource = r;
+        this.name = name;
+        this.detail = detail;
+        this.photo = photo;
+    }
+
+    public AnimalModel(){}
+
+    public boolean isLiked() {
+        return liked;
+    }
+
+    public void setLiked(boolean liked) {
+        this.liked = liked;
+    }
+
+    public int getResource() {
+        return resource;
+    }
+
+    public void setResource(int resource) {
+        this.resource = resource;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+
+    }
+}
---
+++ app/src/main/java/com/nguyennt/animal/util/MyService.java
@@ -0,0 +1,59 @@
+package com.nguyennt.animal.util;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.nguyennt.animal.util.BroadcastReceiver;
+
+public class MyService extends Service {
+    private String LOG_TAG = "MyService";
+    private String myString;
+    private final BroadcastReceiver receiver = new BroadcastReceiver();
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(LOG_TAG, "onCreate MyService");
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(TelephonyManager.EXTRA_STATE);
+
+        registerReceiver(receiver, filter);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(LOG_TAG, "onStartCommand MyService");
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent broadcastIntent = new Intent();
+                broadcastIntent.setAction(TelephonyManager.EXTRA_STATE);
+                sendBroadcast(broadcastIntent);
+                }
+        }).start();
+        return START_REDELIVER_INTENT;
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "In onDestroy");
+        unregisterReceiver(receiver);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i(LOG_TAG, "In onBind");
+        return null;    }
+}
